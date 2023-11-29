1501fba2f14acd1561bcaf729dec1fd1d93a0d41
ochieng steven otieno <stevenochieng6305@gmail.com>
2023-11-29 18:37:21 +0300

    FIX USER ID

--- db/migrate/20231129094036_create_likes.rb
+++ db/migrate/20231129094036_create_likes.rb
@@ -1,11 +1,11 @@
 class CreateLikes < ActiveRecord::Migration[7.1]
   def change
     create_table :likes do |t|
-      t.integer :author_id
+      t.integer :user_id
       t.integer :post_id
 
       t.timestamps
     end
-    add_index :likes, :author_id
+    add_index :likes, :user_id
   end
 end
