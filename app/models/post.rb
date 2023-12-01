2ce431a2d998e7d7aa2797e56a3390bf5e7009a5
ochieng steven otieno <stevenochieng6305@gmail.com>
2023-12-01 10:40:31 +0300

    ADD DATA

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  def update_author_posts_counter
+    author.update(post_counter: author.posts.count)
+  end
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
