85e04688853535e5b34047c223fa42681fa2b920
ochieng steven otieno <stevenochieng6305@gmail.com>
2023-12-12 13:36:21 +0300

    ADD USERS  CONTROLLER SPECS

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  let(:user) { User.create(name: 'steve', photo: 'https://unsplash.com/photos/F_-0000BxGuVvo', bio: 'Teacher from Mexico.') }
+  let(:post) { Post.create(author: user, title: 'Mr', text: 'Hello all!') }
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before { get users_url }
+
+    it 'renders a successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template('users/index')
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      expect(response.body).to include('<h1>All Users</h1>')
+    end
   end
 end
