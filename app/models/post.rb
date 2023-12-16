098f403957048e8b793bcc9e194e9a4dadbeb5f5
ochieng steven otieno <stevenochieng6305@gmail.com>
2023-12-16 08:07:37 +0300

    fix create post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,24 +1,22 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
   has_many :likes
 
-  after_create :increment_user_posts_counter
-  before_destroy :decrement_user_posts_counter
+  after_initialize :set_default_counters
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
-  def increment_user_posts_counter
-    author.increment!(:posts_counter)
-  end
+  private
 
-  def decrement_user_posts_counter
-    author.decrement!(:posts_counter)
+  def set_default_counters
+    self.comments_counter ||= 0
+    self.likes_counter ||= 0
   end
 end
