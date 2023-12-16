803d3fdf3ab14562d4fbfe11655853546bb451fe
ochieng steven otieno <stevenochieng6305@gmail.com>
2023-12-16 11:00:02 +0300

    add routing to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
-Rails.application.routes.draw do
+`Rails.application.routes.draw do
   root to: 'users#index'  
   resources :users, only: %i(index show) do 
     resources :posts, only: %i(index show)
   end
 end
+`
